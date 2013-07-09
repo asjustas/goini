780e81c8c841265689c525ab4a8f9f09a4357711
Justas Vitaitis <justas@u3.lt>
2013-07-10 01:18:05 +0300

    fixed formating

--- goini.go
+++ goini.go
@@ -1,12 +1,12 @@
 package goini
 
 import (
-	"os"
-	"fmt"
-	"errors"
 	"bufio"
-	"strings"
+	"errors"
+	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
 type errorString struct {
@@ -15,8 +15,8 @@ type errorString struct {
 
 type IniRow struct {
 	section string
-	param string
-	value string
+	param   string
+	value   string
 }
 
 type Ini struct {
@@ -31,11 +31,11 @@ func New(text string) error {
 	return &errorString{text}
 }
 
-func getValue(ini *Ini, section string, param string) string{
+func getValue(ini *Ini, section string, param string) string {
 	for _, element := range ini.elements {
 		if element.section == section && element.param == param {
 			return element.value
-			break;
+			break
 			//fmt.Printf("Results: %v\n", element)
 		}
 	}
@@ -46,7 +46,7 @@ func getValue(ini *Ini, section string, param string) string{
 func Load(conf string) (*Ini, error) {
 	file, err := os.Open(conf)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("error opening file: %v\n",err)) 
+		return nil, errors.New(fmt.Sprintf("error opening file: %v\n", err))
 	}
 
 	defer file.Close()
@@ -57,23 +57,23 @@ func Load(conf string) (*Ini, error) {
 	section := ""
 
 	for scanner.Scan() {
-		line := scanner.Text();
+		line := scanner.Text()
 		line = strings.TrimSpace(line)
 
 		if len(line) > 0 && line[0:1] != ";" {
 			if len(line) >= 3 && line[0:1] == "[" /*&& line[(len(line)-1):1] == "]"*/ {
-				section = line[1:len(line)-1]
-			}else{
+				section = line[1 : len(line)-1]
+			} else {
 				if strings.Contains(line, "=") {
 					KeyVal := strings.Split(line, "=")
 
 					row := IniRow{
-		            	section: section,
-						param: strings.TrimSpace(KeyVal[0]),
-						value: strings.TrimSpace(KeyVal[1]),
-			        }
+						section: section,
+						param:   strings.TrimSpace(KeyVal[0]),
+						value:   strings.TrimSpace(KeyVal[1]),
+					}
 
-			        arr.elements = append(arr.elements, row)
+					arr.elements = append(arr.elements, row)
 				}
 			}
 		}
@@ -91,11 +91,11 @@ func (ini *Ini) Int(section, param string) int {
 
 	if val == "" {
 		return 0
-	}else{
+	} else {
 		i, err := strconv.Atoi(val)
-	    if err != nil {
-	    	return 0
-		}else{
+		if err != nil {
+			return 0
+		} else {
 			return i
 		}
 	}
